c5ac62007309a7472eb787dbaf5ffba6e8a6d4c7
Bilawal Liaqat <bilawal.liaqat@nxb.com.pk>
2018-05-28 14:39:08 +0500

    Text compent created and registered.

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+//
+// AppRegistry.registerComponent('albums', () => App);
+
+// Import a library to hep create a registerComponent
+
+import React from 'react';
+//import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
+
+// Create a registerComponent
+
+const App = () => (
+    <Text> Some Text </Text>
+  );
+
+// Render it to device
 
 AppRegistry.registerComponent('albums', () => App);
