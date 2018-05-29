4a5056b29d282e4c41c9d6d141598e497f9cc1a6
Bilawal Liaqat <bilawal.liaqat@nxb.com.pk>
2018-05-29 13:26:24 +0500

    -Render response data and show listing -Data pass to child through props -Pass component through props

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Card from './Card';
+
+
+const AlbumDetail = (props) => {
+
+  return (
+      <Card>
+      <Text> {props.album.title} </Text>
+      </Card>
+  );
+
+};
+
+export default AlbumDetail;
