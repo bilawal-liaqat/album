dfba1e5335731f51b93a09080d0a66c164ffb734
Bilawal Liaqat <bilawal.liaqat@nxb.com.pk>
2018-05-29 15:09:40 +0500

    -Image loading in listing -Make content listing scrollable using Flex

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,59 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const {title, artist , thumbnail_image , image } = album;
 
   return (
       <Card>
-      <Text> {props.album.title} </Text>
+      <CardSection>
+      <View style = {styles.thumnailContainerStyle}>
+        <Image
+        style = {styles.thumnailStyle}
+        source={{ uri: thumbnail_image }} />
+      </View>
+      <View style = { styles.headerContentStyle}>
+          <Text style = {styles.headerTextStyle}> { title} </Text>
+          <Text> { artist} </Text>
+      </View>
+      </CardSection>
+      <CardSection>
+      <Image
+      style = {styles.imageStyle} 
+      source = {{ uri: image}} />
+      </CardSection>
       </Card>
   );
 
+};
+
+
+const styles = {
+  headerContentStyle: {
+      flexDirection: 'column',
+      justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumnailStyle: {
+    width: 50,
+    height: 50
+  },
+  thumnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
